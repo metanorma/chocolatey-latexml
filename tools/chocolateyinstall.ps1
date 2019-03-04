dde4c04e5633bb56fd48cf7998f11863e93b5497
Alexande B <abobrikovich@gmail.com>
2019-03-04 14:19:20 +0300

    Do cpanm install specific version

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -6,6 +6,6 @@ if (!(Test-Path -Path "$Env:ChocolateyInstall\lib\miktex")) {
    & cinst -y miktex
 }
 
-& cpanm --v LaTeXML
+& cpanm --v LaTeXML@$Env:ChocolateyPackageVersion
 
 Get-Command latexml | Select-Object -ExpandProperty Definition

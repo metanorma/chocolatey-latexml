b4df7c791a25e2cc5f43ba08f5ee34b46c3b7c0f
Alexande B <abobrikovich@gmail.com>
2019-03-05 10:27:58 +0300

    Remove cinst from script

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -1,11 +1,10 @@
 $Strawberry = "C:\Strawberry"
-
-# We need to install 'miktex' manually to make sure that it's installed for 'Strawberry Perl'
 $env:Path = "$Strawberry\c\bin;$Strawberry\perl\site\bin;$Strawberry\perl\bin;$Env:Path"
-if (!(Test-Path -Path "$Env:ChocolateyInstall\lib\miktex")) {
-   & cinst -y miktex
-}
 
-& cpanm --verbose LaTeXML@$Env:ChocolateyPackageVersion
+# We need --notest here because:
+# Difference at line 1 for ****
+#       got : 'kpsewhich: warning: running with administrator privileges'
+#  expected : 'No obvious problems'
+& cpanm --notest --verbose LaTeXML@$Env:ChocolateyPackageVersion
 
 Get-Command latexml | Select-Object -ExpandProperty Definition

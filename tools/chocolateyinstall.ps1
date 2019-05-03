337d926863ff52749505e5752049e26f31e6fa64
Alexande B <abobrikovich@gmail.com>
2019-05-03 21:40:04 +0300

    Add MiKTeX before tests run

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -1,6 +1,6 @@
 $Strawberry = "C:\Strawberry"
 $env:Path = "$Strawberry\c\bin;$Strawberry\perl\site\bin;$Strawberry\perl\bin;$Env:Path"
 
-& cpanm --verbose LaTeXML@$Env:ChocolateyPackageVersion
+& cpanm LaTeXML@$Env:ChocolateyPackageVersion
 
 Get-Command latexml | Select-Object -ExpandProperty Definition

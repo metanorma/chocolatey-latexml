efa2d8b2cef75757697d3353f072526a4b3ef89b
Alexande B <abobrikovich@gmail.com>
2020-05-11 11:49:26 +0300

    Beta release

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -1,6 +1,6 @@
 $Strawberry = "C:\Strawberry"
 $env:Path = "$Strawberry\c\bin;$Strawberry\perl\site\bin;$Strawberry\perl\bin;$Env:Path"
 
-& cpanm LaTeXML@$Env:ChocolateyPackageVersion
+& cpanm git://github.com/brucemiller/LaTeXML.git@$Env:ChocolateyPackageVersion
 
 Get-Command latexml | Select-Object -ExpandProperty Definition

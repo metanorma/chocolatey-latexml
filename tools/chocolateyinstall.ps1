5aecc293fd95f57bbcf8eef23d50412a5c28493c
Alexande B <abobrikovich@gmail.com>
2020-05-11 12:08:38 +0300

    Update version to conform chocolatey requirments

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -1,6 +1,6 @@
 $Strawberry = "C:\Strawberry"
 $env:Path = "$Strawberry\c\bin;$Strawberry\perl\site\bin;$Strawberry\perl\bin;$Env:Path"
 
-& cpanm git://github.com/brucemiller/LaTeXML.git@$Env:ChocolateyPackageVersion
+& cpanm git://github.com/brucemiller/LaTeXML.git@9a0e7dc5
 
 Get-Command latexml | Select-Object -ExpandProperty Definition
